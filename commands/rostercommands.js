6a93a2fd9d29ce1a0dc9ee0921859a0ec813d1aa
Kristinnh14 <kristinnh14@gmail.com>
2023-03-17 00:07:32 +0000

    Updated rostercommands.js to include correct rosters for season 6

--- commands/rostercommands.js
+++ commands/rostercommands.js
@@ -1,32 +1,32 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
-// úrvals deild
+// Úrvals deild
 lava = 'LAVA Esports';
-thor = 'Þór Akureyri';
 breidablik = 'Breiðablik';
-kr = 'KR';
-midnightBulls = 'Midnight Bulls';
-somnio = 'Somnio';
+thor = 'Þór Akureyri';
 t354 = '354 eSports';
 breakingSad = 'Breaking Sad';
-
-// fyrsta deild
-pandaBois = 'Pandabois';
-hvb = 'High-Voltage Bangers';
-rusty = 'Rusty';
-ron = 'RON';
-thorWhite = 'Þór White';
-infinity = '1nfinity';
-blulagoons = 'blulaGOONS';
 pushinP = 'Pushin P';
-biddadeins = 'BÍDDAÐEINS';
-clb = 'Certified Lover Boys';
+sudurtak = 'Suðurtak';
+vikingurO = 'Víkingur Ólafsvík';
 
-// önnur deild TBA
-t354Academy = '354 academy';
-rustyAcademy = 'Rusty Academy';
+// Fyrsta deild
+pandaBois = 'Pandabois';
+infinity = '1nfinity';
+hvb = 'High-Voltage Bangers';
+bringBackSvali = 'Bring Back Svali';
+skagadrengir = 'Skagadrengir';
+t9to5 = '9 til 5';
+bondadurgar = 'Bóndadurgar';
+omon = 'OMON';
+outSpeed = 'OutSpeed Esports'
+svorumStrax = 'Svörum Strax';
 
+// Önnur deild TBA
+
+
+// Players
 const emilvald = { name: 'EmilVald', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/EmilVald/overview)` };
 const vaddimah = { name: 'Vaddimah', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/vaddimah/overview)` };
 const paxole = { name: 'Paxole', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/PaxoleOne/overview)` };
@@ -45,7 +45,7 @@ const jappi = { name: 'jappi', value: `[Tracker](https://rocketleague.tracker.ne
 const jm = { name: 'JM', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/JMtheOG/overview)` };
 const bobbi = { name: 'Bobbi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/CMBobbi/overview)` };
 const haxfadir = { name: 'Haxfaðir', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198284400165/overview)` };
-const snazz = { name: 'Snazz', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/snassi2607/overview)` };
+const snazz = { name: 'Snæsi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561199028133353/overview)` };
 const klobbi = { name: 'Klobbi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198172398909/overview)` };
 const bafga = { name: 'Bafga', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198164429718/overview)` };
 const stormur = { name: 'Stormur', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Pungurinn/overview)` };
@@ -69,7 +69,7 @@ const davidkuld = { name: 'davidkuld', value: `[Tracker](https://rocketleague.tr
 const sirmre = { name: 'SirMrE', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198002261949/overview)` };
 const alfarinn = { name: 'Alfarinn', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/andriasgeir/overview)` };
 const lxi = { name: 'Lxi.', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/lxi./overview)` };
-const jdn = { name: 'Jdn.', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198805970350/overview)` };
+const Bjarni = { name: 'bjarni.', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198805970350/overview)` };
 const enzane = { name: 'ENZane', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/ENZane23/overview)` };
 const gullos = { name: 'Gullos10', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Gullos10/overview)` };
 const magni = { name: 'Magni^_^', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Magni^_^/overview)` };
@@ -83,7 +83,7 @@ const saltskeggur = { name: 'SaltSkeggur', value: `[Tracker](https://rocketleagu
 const presley = { name: 'Presley', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Presley93/overview)` };
 const tonichris = { name: 'Toni Chris', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/toni%20chris/overview)` };
 const penguin = { name: 'PenguinGod ', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/PenguinGod1310/overview)` };
-const pepsicola = { name: 'pepsicola.coca', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/pepsicola.coca/overview)` };
+const stofer = { name: 'stofer.', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/stofer./overview)` };
 const drbamboo = { name: 'Dr Bamboo', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/DrBamboo/overview)` };
 const eagle = { name: 'Eagle', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/TheEaglesStream/overview)` };
 const joibje = { name: 'joibje ', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/joibje/overview)` };
@@ -101,7 +101,7 @@ const icenogla = { name: 'IceNogla', value: `[Tracker](https://rocketleague.trac
 const skorrz00 = { name: 'Skorrz00', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/psn/Skorrz00/overview)` };
 const eli = { name: 'Éli', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Daninn/overview)` };
 const uglan = { name: 'Uglan', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Nj%C3%B3sna%20Ugla/overview)` };
-const nokkvi = { name: 'Nökkvi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/n%C3%B6kkvi./overview)` };
+const djNizzy = { name: 'DJ Nizzy', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/dj%20nizzy/overview)` };
 const jcb = { name: 'JcB_RL', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/JcB_RL/overview)` };
 const krissiagust = { name: 'KrissiAgust', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Krissi99/overview)` };
 const ausa = { name: 'Ausa', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Ausa./overview)` };
@@ -111,6 +111,12 @@ const cynical = { name: 'Cynical', value: `[Tracker](https://rocketleague.tracke
 const curli = { name: 'Curli', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561197996734709/overview)` };
 const gummi = { name: 'Gummi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561197982926481/overview)` };
 const hodge = { name: 'Hodge', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/hodge_/overview)` };
+const cryypto = { name: 'cryypto', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198354807974/overview)` };
+const rooneybangzon = { name: 'RooneyBangzon', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Warmfox15/overview)` };
+const justus = { name: 'Justus', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/justusplayer/overview)` };
+const simmivalur = { name: 'SimmiValur', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198834358553/overview)` };
+const mundi = { name: 'Mundi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198200985094/overview)` };
+const tossis = { name: 'Tossis', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Tossis/overview)` };
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -121,26 +127,17 @@ module.exports = {
         .setDescription('Liðið sem skoða skal')
         .setRequired(true)
         .addChoice(lava, lava)
-        .addChoice(thor, thor)
         .addChoice(breidablik, breidablik)
-        .addChoice(midnightBulls, midnightBulls)
+        .addChoice(thor, thor)
+        .addChoice(breakingSad, breakingSad)
         .addChoice(t354, t354)
         .addChoice(breakingSad, breakingSad)
-        .addChoice(blulagoons, blulagoons)
         .addChoice(pushinP, pushinP)
-        // .addChoice(pandaBois, pandaBois)
-        // .addChoice(hvb, hvb)
-        // .addChoice(ron, ron)
-        // .addChoice(thorWhite, thorWhite)
-        // .addChoice(infinity, infinity)
-        // .addChoice(biddadeins, biddadeins)
-        // .addChoice(clb, clb)
-        // .addChoice(t354Academy, t354Academy)
-        // .addChoice(rustyAcademy, rustyAcademy)
+        .addChoice(sudurtak, sudurtak)
+        .addChoice(vikingurO, vikingurO)
     ),
   async execute(interaction) {
     let teamName = interaction.options.getString('lið');
-
 
     let url = 'https://rocketleague.is'
     let fields = []
@@ -153,21 +150,21 @@ module.exports = {
         fields = [
           vaddimah,
           bnz,
-          klobbi,
+          oli,
           cynical
         ];
         url = 'https://lavaesports.com/';
-        image = 'https://i.ibb.co/5TY3FL5/Lava-logo-transparent.png';
+        image = 'https://i.ibb.co/3zktBL6/LAVA-1x1.png';
         break;
 
       case thor:
         fields = [
           steb,
           ousic,
-          hemmigumm,
-          kraken
+          dannijuice,
+          cryypto
         ];
-        url = 'https://thorsport.is/';
+        url = 'https://www.thorsport.is/rafithrottir';
         image = 'https://i.ibb.co/0BgDcTy/thor-akureyri-vector-logo.png';
         break;
 
@@ -182,25 +179,14 @@ module.exports = {
         image = 'https://i.ibb.co/2vQsNcS/breidablik.png';
         break;
 
-      case midnightBulls:
-        fields = [
-          bobbi,
-          haxfadir,
-          snazz,
-          tonichris
-        ];
-        url = 'https://www.mbl.is/sport/esport/2021/09/13/nylidar_midnight_bulls_komu_a_ovart/';
-        image = 'https://i.ibb.co/f4Nz5Hf/MB-resized.png';
-        break;
-
       case t354:
         fields = [
-          cereal,
-          bjarnifraendi,
-          jdn,
+          mundi,
+          tossis,
+          bjarni,
           stormur
         ];
-        url = 'https://354gaming.is/';
+        url = 'https://354esports.is/';
         image = 'https://i.ibb.co/L1HvpJ5/354v2-250px.png';
         break;
 
@@ -215,137 +201,38 @@ module.exports = {
         image = 'https://i.ibb.co/D9y4D21/bslogo512.png';
         break;
 
-
-      case blulagoons:
-        fields = [
-          pepsicola,
-          drbamboo,
-          eagle,
-          curli
-        ];
-        url = 'https://rocketleague.is/bluelagoons';
-        image = 'https://i.ibb.co/HBzH5kC/blgn.png';
-        break;
-
       case pushinP:
         fields = [
           roadman,
-          gummi,
+          klobbi,
           aronrafn,
           hodge
         ];
         url = 'https://rocketleague.is/pushin_p';
-        image = 'https://i.ibb.co/QjfXrdr/PushinP.png';
+        image = 'https://i.ibb.co/0sJ8HYB/pushin-p.png';
         break;
 
-      // case pandaBois:
-      //   fields = [
-      //     { name: 'Stuttbuxur', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/gisliorn/overview)` },
-      //     { name: 'mediuMReyr', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/mediumvape/overview)` },
-      //     { name: 'Slapi36', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/slapi36/overview)` },
-      //     { name: 'Ostakaka', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198044314675/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/m9LDJbQ/Pandabois-logo-resized.png';
-      //   break;
+      case sudurtak:
+        fields = [
+          allifret,
+          djNizzy,
+          simmivalur,
+          justus
+        ];
+        url = 'https://rocketleague.is/sudurtak';
+        image = 'https://i.ibb.co/4W1mwPJ/Sudurtak-darkmode.png';
+        break;
 
-      // case hvb:
-      //   fields = [
-      //     { name: 'Tommi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/t0mah4wk/overview)` },
-      //     { name: 'solvigunn', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198072113176/overview)` },
-      //     { name: 'davidkuld', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/psn/Davidkuld/overview)` },
-      //     { name: 'SirMrE', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198002261949/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/LgL0zXw/HVB2.png';
-      //   break;
-
-      // case rusty:
-      //   fields = [
-      //     { name: 'Alfarinn', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/andriasgeir/overview)` },
-      //     { name: 'Lxi.', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/lxi./overview)` },
-      //     { name: 'vJaden', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198805970350/overview)` },
-      //     { name: 'ENZane', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/ENZane23/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/cD7F7yL/Rusty.png';
-      //   break;
-
-      // case ron:
-      //   fields = [
-      //     { name: 'Gullos10', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Gullos10/overview)` },
-      //     { name: 'Magni^_^', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Magni^_^/overview)` },
-      //     { name: 'Litla_Tikin', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Litla_Tikin/overview)` },
-      //     { name: 'ómó', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/ómó/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/njKVjpf/44b9f8e9-15cf-4b5c-a055-08d8c46e1444-128-128.jpg';
-      //   break;
-
-      // case thorWhite:
-      //   fields = [
-      //     { name: 'Wombo Combo', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/wombocomboturbo/overview)` },
-      //     { name: 'Vikt_ormar', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198988527025/overview)` },
-      //     { name: 'ebbibolla', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/ebbibolla/overview)` },
-      //     { name: 'A1_vexinate', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198860118038/overview)` },
-      //   ];
-      //   url = 'https://thorsport.is/';
-      //   image = 'https://i.ibb.co/0BgDcTy/thor-akureyri-vector-logo.png';
-      //   break;
-
-      // case infinity:
-      //   fields = [
-      //     { name: 'SaltSkeggur', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/SaltSkeggur/overview)` },
-      //     { name: 'Presley', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Presley93/overview)` },
-      //     { name: 'Toni Chris', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/toni%20chris/overview)` },
-      //     { name: 'PenguinGod ', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/PenguinGod1310/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/r7SW5Xc/infinity.png';
-      //   break;
-
-      // case biddadeins:
-      //   fields = [
-      //     { name: 'Ageir9', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Ageir9/overview)` },
-      //     { name: 'Fanarito', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Fanarito/overview)` },
-      //     { name: 'Dairy Free', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/iLikeCaves/overview)` },
-      //     { name: 'Sofasett ', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Sofasett/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/zRpGKw0/B-dda-eins.png';
-      //   break;
-
-      // case clb:
-      //   fields = [
-      //     { name: 'AYOBABE', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/AYOBABE/overview)` },
-      //     { name: 'CertifiedNaldo', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/psn/CertifiedNaldo/overview)` },
-      //     { name: 'IceNogla', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/psn/IceNogla./overview)` },
-      //     { name: 'Skorrz00', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/psn/Skorrz00/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   break;
-
-      // case t354Academy:
-      //   fields = [
-      //     { name: 'Éli', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/Daninn/overview)` },
-      //     { name: 'Uglan', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Nj%C3%B3sna%20Ugla/overview)` },
-      //     { name: 'Nökkvi', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/n%C3%B6kkvi./overview)` },
-      //     { name: 'JcB_RL', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/JcB_RL/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/0Z4tgkK/354-academy-250.png';
-      //   break;
-
-      // case rustyAcademy:
-      //   fields = [
-      //     { name: 'KrissiAgust', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Krissi99/overview)` },
-      //     { name: 'Ausa', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/epic/Ausa./overview)` },
-      //     { name: 'Ingzinn', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/76561198024583791/overview)` },
-      //     { name: 'Krókur', value: `[Tracker](https://rocketleague.tracker.network/rocket-league/profile/steam/KrókurRl/overview)` },
-      //   ];
-      //   url = 'https://rocketleague.is';
-      //   image = 'https://i.ibb.co/cD7F7yL/Rusty.png';
-      //   break;
+      case vikingurO:
+        fields = [
+          gullos,
+          stofer,
+          snazz,
+          rooneybangzon
+        ];
+        url = 'https://rocketleague.is/vikingur-olafsvik';
+        image = 'https://i.ibb.co/gW2rqV6/Vikingur-Olafsvik-allmode.png';
+        break;
 
       // default:
       // // code block
